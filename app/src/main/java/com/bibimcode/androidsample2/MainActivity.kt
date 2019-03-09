53b465b21fbc6302527aafe347f4376e071d0795
ssm-incredible <sungmok.sohn@gmail.com>
2019-03-09 15:53:50 +0900

    다른 액티비티에 입력한 값 표시 성공

--- app/src/main/java/com/bibimcode/androidsample2/MainActivity.kt
+++ app/src/main/java/com/bibimcode/androidsample2/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.bibimcode.androidsample2
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
-
+    companion object {
+        val EXTRA_MESSAGE = "com.bibimcode.androidsample2.MESSAGE"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,6 +18,10 @@ class MainActivity : AppCompatActivity() {
 
     /** Called when the user taps the Send button  */
     fun sendMessage(view: View) {
-        // Do something in response to button
+        val intent = Intent(this, DisplayMessageActivity::class.java)
+        val editText = findViewById<View>(R.id.editText) as EditText
+        val message = editText.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
+        startActivity(intent)
     }
 }
